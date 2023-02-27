deb8d538fd6520f7c075a7d86a1c4a9c9e785717
ArtemisZeep <aazemlyanukhin@gmail.com>
2023-02-27 21:59:40 +0300

    hotfixes: after 1st review

--- scripts/index.js
+++ scripts/index.js
@@ -1,53 +1,35 @@
 const editProfileButton = document.querySelector('.profile__edit-button');
 const editPopup = document.querySelector('.popup');
-
-
-editProfileButton.addEventListener('click', function () {
-  editPopup.classList.add('popup_opened');
-});
-
 const closePopupButton = document.querySelector('.popup__close');
 
-closePopupButton.addEventListener('click', function() {
-  editPopup.classList.remove('popup_opened');
-})
-
-
-let profileName = 'Жак-Ив Кусто';
-let profileStatus = 'Исследователь океана';
-
 let profileNameElement= document.querySelector('.profile__name');
-profileNameElement.textContent = profileName;
 
 let profileStatusElement = document.querySelector('.profile__status');
-profileStatusElement.textContent = profileStatus
 
+let profileNameInput = document.querySelector('.popup__input_text-name');
 
-
-let profileNameInput = document.querySelector('.popup__input-name');
-profileNameInput.value = profileName
-
-let profileStatusInput = document.querySelector('.popup__input-status');
-profileStatusInput.value = profileStatus
-
-
+let profileStatusInput = document.querySelector('.popup__input_text-status');
 
 let formElement = document.querySelector('.popup__form');
+
+function popupOpen () {
+  editPopup.classList.add('popup_opened');
+  profileNameInput.value = profileNameElement.textContent
+  profileStatusInput.value = profileStatusElement.textContent
+}
+
+editProfileButton.addEventListener('click', popupOpen);
+
+function popupClose () {
+  editPopup.classList.remove('popup_opened');
+}
+
+closePopupButton.addEventListener('click', popupClose)
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileNameElement.textContent = profileNameInput.value
   profileStatusElement.textContent = profileStatusInput.value
-}
-
-formElement.addEventListener('submit', handleFormSubmit); 
-
-
-
-
-const submitPopupButton = document.querySelector('.popup__submit-button')
-
-submitPopupButton.addEventListener('click', function() {
   editPopup.classList.remove('popup_opened');
-
-})
+}
+formElement.addEventListener('submit', handleFormSubmit); 
